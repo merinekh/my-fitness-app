32ddef9cb00b8f58f9a74e6855e08b482267909e
Khalil Merine <merinekh@gmail.com>
2023-02-09 16:29:51 -0500

    Add button to Save

--- src/pages/Pages/Exercices/Exercices.js
+++ src/pages/Pages/Exercices/Exercices.js
@@ -2,37 +2,43 @@ import React, { useEffect, useState } from "react";
 import "./Exercices.scss";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import listOfEquipments from "../../../data/listOfEquipment.json";
+import allExercices from "../../../data/AllExercices.json";
 
 function ExercicesPage() {
   const params = useParams();
-  const [data, setData] = useState([]);
+  const allExercicesData = allExercices;
+  const [data, setData] = useState(allExercicesData);
   const [equipment, setEquipment] = useState([]);
-  const [equipments, setEquipments] = useState([]);
+  const [equipments, setEquipments] = useState(listOfEquipments);
 
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://exercisedb.p.rapidapi.com/exercises/equipmentList",
-      headers: {
-        "X-RapidAPI-Key": "659442a743mshc4062bb3c2f725fp146520jsn7554ae154544",
-        "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-      },
-    };
+  // useEffect(() => {
+  //   const options = {
+  //     method: "GET",
+  //     url: "https://exercisedb.p.rapidapi.com/exercises/equipmentList",
+  //     headers: {
+  //       "X-RapidAPI-Key": "659442a743mshc4062bb3c2f725fp146520jsn7554ae154544",
+  //       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  //     },
+  //   };
 
-    axios
-      .request(options)
-      .then(function ({ data }) {
-        setEquipments(data);
-        setEquipment(data[0]);
-        // console.log(data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+  //   axios
+  //     .request(options)
+  //     .then(function ({ data }) {
+  //       setEquipments(data);
+  //       setEquipment(data[0]);
+  //       // console.log(data);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
 
-    // console.log(data);
-  }, []);
-  // console.log(equipment);
+  //   // console.log(data);
+  // }, []);
+  // // console.log(equipment);
+  // setEquipments(listOfEquipments);
+  // setEquipment(data[0]);
+  // console.log(allExercices);
   // =======================Render Equipments========================================
   const dropDown = equipments.map((element, i) => (
     <option key={i} value={element} className="exercices-dropdown__option">
@@ -44,25 +50,33 @@ function ExercicesPage() {
 
   // ========================Onclick Filter=========================================
   const handleSelect = (e) => {
-    const options = {
-      method: "GET",
-      url: `https://exercisedb.p.rapidapi.com/exercises/equipment/${e}`,
-      headers: {
-        "X-RapidAPI-Key": "659442a743mshc4062bb3c2f725fp146520jsn7554ae154544",
-        "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-      },
-    };
-    axios
-      .request(options)
-      .then(function ({ data }) {
-        setData(data);
-        // console.log(data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    // const options = {
+    //   method: "GET",
+    //   url: `https://exercisedb.p.rapidapi.com/exercises/equipment/${e}`,
+    //   headers: {
+    //     "X-RapidAPI-Key": "659442a743mshc4062bb3c2f725fp146520jsn7554ae154544",
+    //     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    //   },
+    // };
+    // axios
+    //   .request(options)
+    //   .then(function ({ data }) {
+    //     setData(data);
+    //     // console.log(data);
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
+    const dataFilter = allExercices.filter((element) => {
+      if (element.equipment === e) {
+        return element;
+      } else {
+        return "not matching";
+      }
+    });
+    setData(dataFilter);
   };
-  console.log(data);
+  // console.log(data);
   const card = () => {
     return data.map((element) => {
       return (
@@ -89,6 +103,8 @@ function ExercicesPage() {
               </h3>
             </div>
           </div>
+
+          <button className="exercices-card__button">Save</button>
         </div>
       );
     });
@@ -98,7 +114,7 @@ function ExercicesPage() {
     <div className="exercices">
       <select
         className="exercices-dropdown"
-        onChange={(e) => handleSelect(e.target.value.replace(/ /g, "%20"))}
+        onChange={(e) => handleSelect(e.target.value)}
       >
         <option value=""></option>
         {dropDown}
@@ -106,28 +122,34 @@ function ExercicesPage() {
 
       <div
         id="myCarousel"
-        class="carousel slide container"
+        className="carousel slide container"
         data-bs-ride="carousel"
       >
         <button
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           type="button"
           data-bs-target="#myCarousel"
           data-bs-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <div className="exercices-cards carousel-inner">{card()}</div>
 
         <button
-          class="carousel-control-next"
+          className="carousel-control-next"
           type="button"
           data-bs-target="#myCarousel"
           data-bs-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
     </div>
